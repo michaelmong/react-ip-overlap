6eb0cc6f06715eded68fa9bbf2ee1687dfde7fd6
michaelmong <50898836+michaelmong@users.noreply.github.com>
2020-04-21 00:07:25 +0700

    app.yaml added

---
+++ build/precache-manifest.330e16d1635b9d8045d235a8fea973d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeaee5696206fc5ca1d217a36e7f5c7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "483166a2f6f088369c04",
+    "url": "/static/css/main.c7020da3.chunk.css"
+  },
+  {
+    "revision": "def59089ac924f0cead0",
+    "url": "/static/js/2.8719477d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8719477d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "483166a2f6f088369c04",
+    "url": "/static/js/main.6d9b4ece.chunk.js"
+  },
+  {
+    "revision": "655391a53b07eb9ae621",
+    "url": "/static/js/runtime-main.5958040f.js"
+  }
+]);
